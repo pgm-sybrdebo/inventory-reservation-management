8cb61e1ab8577cb5c6e3b44fc0664f0b194086d5
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2022-01-17 23:34:52 +0100

    added search bar and update functionality - fixes #54

---
+++ src/client/src/components/dashboard/columns/columnsDeviceStatus.tsx
@@ -0,0 +1,108 @@
+// import { GridColDef, } from '@material-ui/data-grid';
+import { GridColDef} from '@mui/x-data-grid';
+import styled from 'styled-components';
+import { Delete, DeleteForever, VisibilityOff } from '@material-ui/icons';
+import { BiEdit } from "react-icons/bi";
+import moment from "moment";
+
+const Button = styled.button`
+  width: 2rem;
+  height: 2rem;
+  min-width: 2rem;
+  min-height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #CBBEC5;
+  border: 1px solid #CBBEC5;
+  background-color: transparent;
+  color: #F58732;
+  border: 1px solid #F58732;
+  border-radius: 3px;
+  cursor:pointer;
+  font-size: 1rem;
+  font-weight: bold;
+  transition: all 0.2s ease-in-out;
+  margin: 0 auto;
+
+  &:hover {
+    background-color: #F58732;
+    color: #FFF;
+    // border: 1px solid #F58732;
+
+  }
+`;
+
+const columnsDeviceStatus: GridColDef[] = [
+  { field: "name", headerName: "Name", width: 400 },
+  { 
+    field: "created_on", 
+    headerName: "Created on", 
+    width: 150,
+    renderCell: (params) => {
+      const d = new Date(Number(params.row.created_on));
+      const dString = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+      const date = moment(dString).format("DD-MM-YYYY");
+      return (
+        <span>{date}</span>
+      )
+    }
+  },
+  { 
+    field: "updated_on", 
+    headerName: "Updated on", 
+    width: 150,
+    renderCell: (params) => {
+      const d = new Date(Number(params.row.updated_on));
+      const dString = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+      const date = moment(dString).format("DD-MM-YYYY");
+      return (
+        <span>{date}</span>
+      )
+    }
+  },
+  {
+    field: "edit",
+    headerName: "Edit",
+    width: 100,
+    sortable: false,
+    renderCell: (params) => {
+
+      return (
+        <Button>
+          <BiEdit />
+        </Button>
+      );
+    },
+  },
+  {
+    field: "softDelete",
+    headerName: "Soft Delete",
+    width: 100,
+    sortable: false,
+    renderCell: (params) => {
+
+      return (
+        <Button>
+          <Delete />
+        </Button>
+      );
+    },
+  },
+  {
+    field: "delete",
+    headerName: "Hard Delete",
+    width: 100,
+    sortable: false,
+    renderCell: (params) => {
+
+      return (
+        <Button>
+          <DeleteForever />
+        </Button>
+      );
+    },
+  },
+];
+
+export { columnsDeviceStatus };
---
+++ src/client/src/components/dashboard/createForms/CreateFormDeviceStatus.tsx
@@ -0,0 +1,143 @@
+import { useMutation } from '@apollo/client';
+import React from 'react'
+import styled from 'styled-components';
+import * as yup from 'yup'
+import { CREATE_DEVICE_STATUS, GET_ALL_DEVICE_STATUSES_BY_NAME_WITH_PAGINATION, TOTAL_DEVICE_STATUSES_BY_NAME } from '../../../graphql/deviceStatuses';
+import { Button, Dialog, DialogContent, DialogTitle, Grid, MenuItem, Select, TextField } from '@material-ui/core';
+import { Formik, Form, Field } from 'formik'
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 3rem 0;
+`;
+
+interface CreateFormDeviceStatusProps {
+  open: boolean,
+  handleClose: any
+}
+
+const validationSchema = yup.object({
+  name: yup.string().min(1).required("Required")
+})
+
+const CreateFormDeviceStatus = ({open, handleClose }: CreateFormDeviceStatusProps ) => {
+  const [createDeviceStatus] = useMutation(CREATE_DEVICE_STATUS);
+
+  return (
+    <Dialog 
+      fullWidth
+      open={open}
+      onClose={handleClose}
+    >
+      <>
+        <DialogTitle>
+          Create new device status
+        </DialogTitle>
+        <DialogContent>
+          <Formik
+            initialValues={{
+              name: "",
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+              setSubmitting(true);
+              try {
+                console.log("create");
+                await createDeviceStatus({
+                  variables: {
+                    name: values.name
+                  }, 
+                  refetchQueries: [
+                    {
+                      query: GET_ALL_DEVICE_STATUSES_BY_NAME_WITH_PAGINATION,
+                      variables: {
+                        name: "",
+                        offset: 0,
+                        limit: 10
+                      }
+                    },
+                    {
+                      query: 
+                      TOTAL_DEVICE_STATUSES_BY_NAME,
+                      variables: {
+                        name: "",
+                      }
+                    }
+                  ]
+                });
+                console.log("done");
+                handleClose();
+              } catch (error) {
+                console.log(error);
+              }
+              setSubmitting(false);
+
+            }}
+            validationSchema={validationSchema}
+          > 
+            {({
+              handleSubmit,
+              isSubmitting,
+              handleChange,
+              submitForm,
+              setFieldValue,
+              values,
+              touched,
+              errors
+            }) => (
+              <Form>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Field 
+                      component={TextField}
+                      fullWidth
+                      name="name"
+                      type="text"
+                      label="Name:"
+                      value={values.name}
+                      // @ts-ignore
+                      onChange={(e) => {setFieldValue("name", e.target.value)}}
+                      error={Boolean(touched.name && errors.name)}
+                      helperText={touched.name && errors.name}
+                    />
+                  </Grid>
+                </Grid>
+                <ButtonContainer>
+                  <Button
+                    // type='submit'
+                    variant='contained'
+                    size='large'
+                    fullWidth
+                    disabled={isSubmitting}
+                    onClick={submitForm}
+                    style={{
+                      backgroundColor: '#F58732',
+                      marginRight: '3rem'
+                    }}
+                  >
+                    Create
+                  </Button>
+                  <Button
+                    onClick={handleClose}
+                    variant='outlined'
+                    size='large'
+                    fullWidth
+                    style={{
+                      borderColor: '#ED0034',
+                      borderWidth: '2px' 
+                  }}
+                  >
+                    Cancel
+                  </Button>
+                </ButtonContainer>
+              </Form>
+            )}     
+          </Formik>
+        </DialogContent>
+      </>
+    </Dialog>
+  )
+}
+
+export default CreateFormDeviceStatus
