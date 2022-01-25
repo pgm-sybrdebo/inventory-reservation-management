223b715b7f726ecb64700a12781a91e734045e1f
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2022-01-25 20:38:23 +0100

    redeploy

--- src/server/src/database/connection.ts
+++ src/server/src/database/connection.ts
@@ -9,22 +9,22 @@ const nodeEnvironment = `${(
 
 const typeormConfig: ConnectionOptions = {
   type: 'postgres',
-  host: 'frankfurt-postgres.render.com',
-  port: 5432,
-  username: 'aidsyb',
-  password: 'K3W1heJnIprF5WFpzqeQSU6EEuMEQRjj',
-  database: 'artevelde_inventory',
-  // host: 'ec2-54-162-211-113.compute-1.amazonaws.com',
+  // host: 'frankfurt-postgres.render.com',
   // port: 5432,
-  // username: 'spftdcheqzbjrb',
-  // password: '2def93e13d0217714916d7ecc80d92ab2e1e9376a570b95253674362b41afcc2',
-  // database: 'ddv7il8pg12ett',
+  // username: 'aidsyb',
+  // password: 'K3W1heJnIprF5WFpzqeQSU6EEuMEQRjj',
+  // database: 'artevelde_inventory',
+  host: 'ec2-54-162-211-113.compute-1.amazonaws.com',
+  port: 5432,
+  username: 'spftdcheqzbjrb',
+  password: '2def93e13d0217714916d7ecc80d92ab2e1e9376a570b95253674362b41afcc2',
+  database: 'ddv7il8pg12ett',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: nodeEnvironment === 'development' ? true : false,
   dropSchema: nodeEnvironment === 'test' ? true : false,
-  ssl: true,
-  // ssl: { rejectUnauthorized: false }
+  // ssl: true,
+  ssl: { rejectUnauthorized: false }
 };
 
 // important to work with CLI
--- src/server/src/database/factory/user.factory.ts
+++ src/server/src/database/factory/user.factory.ts
@@ -3,14 +3,16 @@ import * as Faker from 'faker';
 import { User } from 'src/users/entities/user.entity';
 import * as bcrypt from 'bcrypt';
 
+
 define(User, (faker: typeof Faker) => {
+ 
   const user = new User();
   user.firstName = faker.name.firstName();
   user.lastName = faker.name.lastName();
   user.email = faker.internet.email();
-  user.password = `${bcrypt.hash('password', 10)}`;
+  // @ts-ignore
+  user.password = bcrypt.hash('password', 10);
   user.cardNumber = faker.random.number({ min: 10000, max: 99999 });
-  user.role = 0;
   user.profession = faker.random.number({ min: 0, max: 1 });
   return user;
 });
